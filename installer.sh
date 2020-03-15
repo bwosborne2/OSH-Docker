dff2ceea5b5bb225659999e8e80778fb0e240b1d
bwosborne2 <51490785+bwosborne2@users.noreply.github.com>
2020-03-15 09:49:31 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -13,7 +13,9 @@ SILENT=false
 
 
 userCheck() {
-    sudo useradd -d /opt/openhab -m -r -s /sbin/nologin openhab
+#    sudo useradd -d /opt/openhab -m -r -s /sbin/nologin openhab
+    sudo groupadd -g 9001 -r openhab
+    sudo useradd -d /opt/openhab -u 9001 -g 9001  -m -r -s /sbin/nologin openhab
     ID=`id -u openhab`
     GR=`id -g openhab`
     echo -e "User: $ID"
