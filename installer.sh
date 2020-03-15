db4111139269387329b56c4ae5bcaf641e777d05
bwosborne2 <51490785+bwosborne2@users.noreply.github.com>
2020-03-15 09:20:34 -0400

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+SCRIPT_VERSION=.0.0.1
+
+GREY_RED='\033[0;37;41m'
+GREEN_DARK='\033[0;32;40m'
+BLUE_DARK='\033[1;34;40m'
+BLACK_WHITE='\033[1;30;47m'
+RED_WHITE='\033[1;31;47m'
+BLINKING='\033[5;37;41m'
+NC='\033[0m' # Reset
+
+SILENT=false
+
+
+userCheck() {
+    sudo useradd -d /opt/openhab -m -r -s /sbin/nologin openhab
+    ID=`id -u openhab`
+    GR=`id -g openhab`
+    echo -e "User: $ID"
+    echo -e "Group: $GR"
+
+}
+
+dataDirs() {
+    sudo mkdir /opt/openhab/conf
+    sudo mkdir /opt/openhab/userdata
+    sudo mkdir /opt/openhab/addons
+    sudo chown -R openhab:openhab /opt/openhab
+
+}
+
+
+userCheck
+dataDirs
