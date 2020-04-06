60833df189f83c4c4584d4876e1820b752b45dc7
bwosborne2 <51490785+bwosborne2@users.noreply.github.com>
2020-04-06 19:13:54 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -44,8 +44,6 @@ dockerService() {
     echo "[Info] Install openHAB startup scripts"
     curl -sL  ${URL_DOCKER_COMPOSE} > /opt/openhab/docker/docker-compose.yml
     curl -sL  ${URL_SERVICE_OPENHAB} > /etc/systemd/system/openhab.service
-    curl -sL  ${URL_SCRIPT_OPENHABCLOUD} > /opt/openhab/etc/cont-init.d/40-openhabcloud
-    chmod +x /opt/openhab/etc/cont-init.d/40-openhabcloud
 
     systemctl daemon-reload
     systemctl enable openhab.service
